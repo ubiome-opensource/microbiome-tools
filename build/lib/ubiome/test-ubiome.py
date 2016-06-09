0cbfebc007d27e21996a9fdd1f864292facaad8b
Richard Sprague <richardsprague@gmail.com>
2016-06-09 13:57:53 -0700

    prep for adding to Pypi

---
+++ build/lib/ubiome/test-ubiome.py
@@ -0,0 +1,119 @@
+__author__ = 'sprague'
+
+import unittest
+import datetime
+
+
+
+
+# I recognize it's not ideal to import straight from the current directory
+# eventually all tests should be moved out of the package directory, or find a better way to do this.
+import ubiome
+
+import ubiomeMultiSample
+
+#from ..ubiome import UbiomeMultiSample
+
+pathPrefix="./testdata/"
+s1 = ubiome.UbiomeSample(pathPrefix+"sample1.json",name="sample1")
+s2 = ubiome.UbiomeSample(pathPrefix+"sample2.json",name="sample2")
+s3 = ubiome.UbiomeSample(pathPrefix+"sample3.json",name="sample2")
+s4 = ubiome.UbiomeSample(pathPrefix+"sample4.json",name="sample2")
+may14 = ubiome.UbiomeSample(pathPrefix+"Sprague-ubiomeMay2014.json",name="May 2014")
+jun14 = ubiome.UbiomeSample(pathPrefix+"sprague-uBiomeJun2014.json",name="Jun 2014")
+aug15 = ubiome.UbiomeSample(name="Aug 2015")
+aug15.read_CSV_file(pathPrefix + "Sprague-ubiome-150815.csv")
+aug = aug15 #ubiome.UbiomeSample(pathPrefix+"Sprague-ubiome-150815.json",name = "Aug 2015")
+
+
+class MyTestCase(unittest.TestCase):
+
+    def setUp(self):
+        x = ubiomeMultiSample.UbiomeMultiSample(may14)
+        x.merge(jun14)
+        x.merge(aug)
+        self.sampleMultiSample = x
+
+    # TODO  fix Ubiome.UbiomeMultiSample to handle first init properly
+    # def test_multiSample_init(self):
+    #     newMS = ubiome.UbiomeMultiSample()
+    #     newMS.merge(jun14)
+    #
+
+
+    def test_unique(self):
+        v = may14.unique(jun14)
+        self.assertEqual(len(v.sampleList), 384)
+    def test_compare_with(self):
+        v = may14.compareWith(jun14)
+        self.assertEqual(len(v.sampleList),139)
+
+    def test_taxList_count_norm_is_integer(self):
+        v = may14._taxaList[0].count_norm
+        self.assertIsInstance(v,int)
+
+    def test_sort_keeps_length_constant(self):
+        v = len(s1._taxaList)
+        s1.sort("count_norm")  # remember that sort is mutable.
+        self.assertEqual(len(s1._taxaList),v)
+
+    def test_addCountsToList(self):
+        v = may14.addCountsToList(["Roseburia","Akkermansia"])
+        self.assertEqual(v,[{'tax_name': 'Roseburia', 'count_norm': 13554}, {'tax_name': 'Akkermansia', 'count_norm': 30960}])
+
+    def test_CountNormOf(self):
+        v = may14.countNormOf("Clostridiales")
+        self.assertEqual(v,594169)
+
+    def test_diversity(self):
+        v = s1.diversity()
+        self.assertEqual(v,0.8565147480876362)
+
+    def test_diversity_rank_genus(self):
+        v = s1.diversity(rank="genus")
+        self.assertEqual(v,0.2064177920125012)
+
+    def test_taxonOf(self):
+        v = may14.taxonOf("Clostridiales")
+        self.assertEqual(v,{'taxon': '186802', 'count': 150137, 'tax_rank': 'order', 'avg': None, 'tax_color': None, 'tax_name': 'Clostridiales', 'parent': '186801', 'count_norm': 594169})
+
+    def test_sampleFullTaxListLength(self):
+        self.assertEqual(len(self.sampleMultiSample.fullTaxList), 795)
+    def test_sampleTaxList(self):
+        firstTenVals = [['tax_name', 'tax_rank'], ['Bacteria', 'superkingdom'], ['Firmicutes', 'phylum'], ['Clostridia', 'class'], ['Clostridiales', 'order'], ['Bacteroidetes/Chlorobi group', 'superphylum'], ['Bacteroidetes', 'phylum'], ['Bacteroidia', 'class'], ['Bacteroidales', 'order'], ['Ruminococcaceae', 'family']]
+        self.assertEqual(firstTenVals,[self.sampleMultiSample.fullTaxList[i] for i in range(10)])
+
+    def test_latestSample(self):
+        latestSample = ['Aug 2015', 1000000, 642701, 622361, 622244, 184516, 184508, 184204, 184136, 307556]
+        self.assertEqual(latestSample,[self.sampleMultiSample.samples[len(self.sampleMultiSample.samples)-1][i] for i in range(10)])
+
+    def test_sort(self):
+        self.assertEqual(jun14.taxaList[-1].tax_name,'Anaerostipes hadrus')
+        jun14.sort("tax_name")
+        v = jun14.taxaList[-1].tax_name
+        self.assertEqual(v,'Achromobacter')
+
+    def test_multisample_init(self):
+        v = ubiomeMultiSample.UbiomeMultiSample(may14)
+        self.assertEqual(len(v.fullTaxList),len(may14.taxaList)+1)
+        self.assertEqual(v.fullTaxList[57][0],may14.taxaList[56].tax_name) # +1 because fullTaxList[0]="tax_rank"
+
+    def test_alltaxa_isList(self):
+        self.assertEqual(len(self.sampleMultiSample.alltaxa()),795)
+
+    def test_found_metadata(self):
+        self.assertEqual(s3.datetime, datetime.datetime(2016,1,11,0,0))
+        self.assertEqual(s3.site,"mouth")
+        self.assertEqual(s2.site,"gut")
+        self.assertEqual(s1.datetime,datetime.datetime(2000,1,1,0,0))
+        self.assertEqual(s1.date, datetime.date(2000, 1, 1))
+        self.assertEqual(s4.date, datetime.date(2015, 8, 22))
+        self.assertEqual(s4.datetime,datetime.datetime(2015,8,22,3,0))
+
+    def tearDown(self):
+        pass
+
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ build/lib/ubiome/ubiomeMultiSample.py
@@ -0,0 +1,144 @@
+
+import sys
+import csv
+from ubiome import UbiomeSample
+
+class UbiomeMultiSample(object):
+    """
+    merge a bunch of samples into a single data frame called 'samples'
+    [ fullTaxList, sample1Quantities, sample2Quantities, ... ]
+
+    fullTaxList is a list containing strings of tax_name; fullTaxList[0] = "tax_name"
+    sampleQuantities is a list where sampleQuantities[0] = "July"  and [1..n] correspond to Quants for fullTaxList[1..n]
+
+    usage: (assuming sample1 and sample2 are of class UbiomeSample)
+
+    x = UbiomeMultiSample()  # initializing
+    x.merge(sample1) #
+
+
+
+
+    """
+    def __init__(self,newSample = []):
+        """
+        : param newSample: UbiomeSample.UbiomeSample
+        :rtype: UbiomeMultiSample
+        """
+        self.fullTaxList = [["tax_name","tax_rank"]]
+        self.samples = []
+        if newSample:
+            self.fullTaxList +=newSample.taxnames()
+#            self.samples+=[[newSample.name]+[sample["count_norm"] for sample in newSample.sampleList]]
+            self.samples+=[[newSample.name]+[sample.count_norm for sample in newSample.taxaList]]
+
+
+    def alltaxa(self):
+        ''' returns just the taxa in this multisample
+
+        :return: list
+        '''
+        alltaxa = []
+        for sample in self.fullTaxList:
+            taxa = sample[0]
+            alltaxa += [taxa]
+        return alltaxa
+
+
+    def showContents(self):
+        print("length of fullTaxList=",len(self.fullTaxList))
+        print([self.fullTaxList[i] for i in range(10)])
+        print("length of samples=",len(self.samples))
+        for sample in self.samples:
+            print(sample[0],"--->",len(sample))
+        print("latest sample:",[self.samples[len(self.samples)-1][i] for i in range(10)])
+
+    def __str__(self):
+        return "UbiomeMultiSample: len=" + str(len(self.fullTaxList)) + "\n" + "latest sample:"+str([self.samples[len(self.samples)-1][i] for i in range(10)])
+
+
+    def merge(self,sample2):
+        """ merge the current multiSample with sample2.  This operation is mutable
+        so you permanently modify the current UbiomeMultiSample when you do this.
+
+        :param sample2: UbiomeSample
+        :return:
+        """
+
+        if len(self.fullTaxList)==1: #special case when there are no existing samples
+            self.fullTaxList +=sample2.taxnames()
+            self.samples+=[[sample2.name]+[sample.count_norm for sample in sample2.taxaList]]
+            return True
+
+        # find the taxNames missing from fullTaxList
+        newTaxNamesL = []
+        sampleTaxNames = sample2.taxnames()
+
+        #Sample2ZippedList = sample2.taxnames()
+        #justSample2TaxNames, justSample2TaxRanks = zip(*Sample2ZippedList)
+       # justFullTaxNames, justFullTaxRanks = zip(*self.fullTaxList)
+
+        #newTaxRanksL = []
+        for i,taxName in enumerate(sampleTaxNames):
+            if taxName not in self.fullTaxList:
+                newTaxNamesL+=[taxName]
+               # newTaxRanksL+=[justFullTaxRanks[i]]
+
+
+
+        self.fullTaxList+=newTaxNamesL
+        newTaxons = [sample2.taxonOf(taxa[0])for taxa in newTaxNamesL]
+
+        #[sample["count_norm"] for sample in sample2.sampleList]
+        oldSamplesList = self.samples[len(self.samples)-1]
+        newSampleCountsForPreviousTaxa = []
+        for i in range(len(oldSamplesList)-1):
+            taxonForTaxName = sample2.taxonOf(self.fullTaxList[i+1][0])
+            if taxonForTaxName:
+                taxCount = taxonForTaxName["count_norm"]
+            else: taxCount = 0
+            newSampleCountsForPreviousTaxa+=[taxCount]
+        newCounts =  newSampleCountsForPreviousTaxa + [taxon["count_norm"] for taxon in newTaxons]
+
+
+
+
+        self.samples += [[sample2.name] + newCounts]
+
+        # new length of a sample is len(newTaxons)+ len(oldSamplesList)
+        # fill previous samples with count_norm = 0
+        for i, sample in enumerate(self.samples):
+            if len(self.samples[i])<(len(newCounts)+1):
+                self.samples[i]=self.samples[i] + [0 for k in range(len(newTaxons))]
+
+
+
+    def writeCSV(self,filename):
+        """ write the merged bunch of sample to a single CSV file (or sys.stdout)
+
+        :param filename:
+        :return:
+        """
+        if filename==sys.stdout:
+            ubiomeWriter = csv.DictWriter(sys.stdout,dialect='excel',fieldnames=["tax_name"]+ ["tax_rank"] + [sample[0] for sample in self.samples])
+            #print('writing to csv')
+            ubiomeWriter.writeheader()
+            for i,taxa in enumerate(self.fullTaxList):
+                taxName, taxRank = taxa
+                rowDict = ["tax_name",taxName]
+                rankDict = ["tax_rank",taxRank]
+                sampleDict = [[sample[0],sample[i]] for sample in self.samples]
+                ubiomeWriter.writerow(dict([rowDict]+[rankDict] +sampleDict))
+        else:
+            with open(filename,'w') as csvFile:
+                #print('writing to csv')
+                ubiomeWriter = csv.DictWriter(csvFile, dialect='excel',fieldnames=["tax_name"]+ ["tax_rank"] + [sample[0] for sample in self.samples])
+                #print('writing to csv')
+                ubiomeWriter.writeheader()
+                for i,taxa in enumerate(self.fullTaxList):
+                    taxName, taxRank = taxa
+                    rowDict = ["tax_name",taxName]
+                    rankDict = ["tax_rank",taxRank]
+                    sampleDict = [[sample[0],sample[i]] for sample in self.samples]
+                    ubiomeWriter.writerow(dict([rowDict]+[rankDict] +sampleDict))
+
