9c69b54e1203300a1003011fecb2591efcff6afa
Richard Sprague <richardsprague@gmail.com>
2016-06-09 17:13:52 -0700

    updated setup files

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
@@ -67,11 +67,11 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-  packages=find_packages(exclude=['docs', 'tests']),
+  packages=['ubiome'],
 
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
-    py_modules=["ubiome.py"],
+    # py_modules=["ubiome.py"],
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
